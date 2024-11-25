7626af81580887d94d45f7b33efb38ca4363b4d8
0HOON <yhoonyh@gmail.com>
2024-11-26 00:30:08 +0900

    navigation update

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ load_dotenv()
 ## TODO review 전부 가져오기
 ## TODO 결과 복사 버튼 만들기
 
-def reset():
-  if st.button("reset"):
-    st.session_state.clear()
+def get_url():
+  st.header("Meta Review Helper :books:")
+  url = st.text_input("Enter OpenReview URL of the paper:")
+  if st.button("Summarize!"):
+    st.session_state.chain = prepare_chain(url)
     st.rerun()
 
-def submit():
-  url = st.session_state.url_input
+def reset():
   st.session_state.clear()
-  st.session_state.chain = prepare_chain(url)
-  st.session_state.url_input = ''
+  st.rerun()
 
 def main():
   # load env settings
@@ -32,27 +32,26 @@ def main():
   # - review/inconsistency/discussion_summary: generated summary text
 
   st.set_page_config(page_title="Meta Review Helper", page_icon=":books:", layout="wide")
-
-  st.write(css, unsafe_allow_html=True)
   
-  with st.sidebar:
-    st.header("Meta Review Helper :books:")
-    st.text_input("Enter OpenReview URL of the paper:", key='url_input', on_change=submit)
-        
+  #st.write(css, unsafe_allow_html=True)
+
+  dashboard_page = st.Page("st_pages/dashboard.py", title="Dashboard", icon=":material/browse_activity:", default=True)
+  review_summary_page = st.Page("st_pages/reviewSummary.py", title="Review Summary", icon=":material/emoji_people:")
+  inconsitency_summary_page = st.Page("st_pages/inconsistencySummary.py", title="Inconsistency Summary", icon=":material/connect_without_contact:")
+  discussion_summary_page = st.Page("st_pages/discussionSummary.py", title="Discussion Summary", icon=":material/sports_kabaddi:")
+  reset_page = st.Page(reset, title="Try Other Paper", icon=":material/search:")
+  
   page_dict = {}
   if "title" in st.session_state:
+    pages = [dashboard_page, review_summary_page, inconsitency_summary_page, discussion_summary_page]
+    page_dict[st.session_state.title] = pages
+
+    pg = st.navigation({"Account": [reset_page]} | page_dict)
     st.title(st.session_state.title)
+  else:
+    pg = st.navigation([st.Page(get_url)])    
 
-  review_summary_page = st.Page("st_pages/reviewSummary.py", title="Review Summary", icon=":material/handyman:", default=True)
-  inconsitency_summary_page = st.Page("st_pages/inconsistencySummary.py", title="Inconsistency Summary", icon=":material/handyman:")
-  discussion_summary_page = st.Page("st_pages/discussionSummary.py", title="Discussion Summary", icon=":material/handyman:")
-
-  pages = [review_summary_page, inconsitency_summary_page, discussion_summary_page]
-  page_dict["Main Functions"] = pages
-
-  reset_page = st.Page(reset, title="reset", icon=":material/logout:")
-  pg = st.navigation({"Settings": [reset_page]} | page_dict)
-  pg.run()        
+  pg.run()
 
   
 
